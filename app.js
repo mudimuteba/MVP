1617220eb95140dd2ee9879f6ba030527c9c106f
decentguy <bludd1one@gmail.com>
2019-03-19 14:01:57 +0200

    and another one

--- app.js
+++ app.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-let orgArr = ['壹','壹','貳','貳','叄','叄','肆','肆'];
-let memValues = [];
-let tileIds = [];
-
-let arrayShuffle = function(arr) {
-    let newPos, temp;
-
-    for(let i = arr.length - 1; i > 0; i--) {
-        newPos = Math.floor(Math.random() * (i+1));
-        temp = arr[i];
-        arr[i] = arr[newPos];
-        arr[newPos] = temp;
-        }
-        return arr;
-};
-
-// let newArray = arrayShuffle(arry);
-// console.log(newArray);
-
-function makeBoard(){
-	tiles_flipped = 0;
-	let output = '';
-  arrayShuffle(orgArr);
-  //for(let i = orgArr.length; i > 0; i--)
-  for(let i = 0; i < orgArr.length; i++)
-  {
-		output += '<div id="tile_'+i+'" onclick="flipper(this,\''+orgArr[i]+'\')"></div>';
-	}
-	document.getElementById('board').innerHTML = output;
-}
-function flipper(tile,val){
-	if(tile.innerHTML == "" && memValues.length < 2){
-		tile.style.background = '#FFF';
-		tile.innerHTML = val;
-		if(memValues.length == 0){
-			memValues.push(val);
-			tileIds.push(tile.id);
-		} else if(memValues.length == 1){
-			memValues.push(val);
-			tileIds.push(tile.id);
-			if(memValues[0] == memValues[1]){
-				tiles_flipped += 2;
-				// Clear both arrays
-				memValues = [];
-        tileIds = [];
-				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
-					alert("Board cleared... generating new board");
-					document.getElementById('board').innerHTML= "";
-					makeBoard();
-				}
-			} else {
-				function flipBack(){
-				    // Flip the 2 tiles back over
-				    let tile_1 = document.getElementById(tileIds[0]);
-				    let tile_2 = document.getElementById(tileIds[1]);
-				    tile_1.style.background = 'url(https://lh3.google.com/u/0/d/14lTcxgGUdiEy0iBPRO81J37jpju3g9_T=w1024-h616-iv1) no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(https://lh3.google.com/u/0/d/14lTcxgGUdiEy0iBPRO81J37jpju3g9_T=w1024-h616-iv1) no-repeat';
-            	    tile_2.innerHTML = "";
-				    // Clear both arrays
-				    memValues = [];
-            	    tileIds = [];
-				}
-				setTimeout(flipBack, 700);
-			}
-		}
-	}
-}
-//makeBoard()
-=======
 var orgArr = ['壹','壹','貳','貳','叄','叄','肆','肆'];
 var memValues = [];
 var tileIds = [];
@@ -117,9 +45,8 @@ function flipper(tile,val){
         tileIds = [];
 				// Check to see if the whole board is cleared
 				if(tiles_flipped == orgArr.length){
-					//alert("Board cleared... generating new board");
-          output2 = '<div id="gameFinished" onclick="flipper(this,\''+orgArr[i]+'\')"></div>';
-					document.getElementById('board').innerHTML = "";
+				alert("Board cleared... generating new board");
+				document.getElementById('board').innerHTML = "";
 					makeBoard();
 				}
 			} else {
@@ -127,9 +54,9 @@ function flipper(tile,val){
 				    // Flip the 2 tiles back over
 				    var tile_1 = document.getElementById(tileIds[0]);
 				    var tile_2 = document.getElementById(tileIds[1]);
-				    tile_1.style.background = 'url(https://lh3.google.com/u/0/d/14lTcxgGUdiEy0iBPRO81J37jpju3g9_T=w1024-h616-iv1) no-repeat';
+				    tile_1.style.background = 'url(https://lh5.googleusercontent.com/vtMb8kt9QYQlfYVXCP0iouDt5qRkdFNrckMyUjvVpj9G9WTfhkvzSp8RZEbmpR5bBHAfC9SS0q1pbw=w1024-h616) no-repeat';
             	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(https://lh3.google.com/u/0/d/14lTcxgGUdiEy0iBPRO81J37jpju3g9_T=w1024-h616-iv1) no-repeat';
+				    tile_2.style.background = 'url(https://lh5.googleusercontent.com/vtMb8kt9QYQlfYVXCP0iouDt5qRkdFNrckMyUjvVpj9G9WTfhkvzSp8RZEbmpR5bBHAfC9SS0q1pbw=w1024-h616) no-repeat';
             	    tile_2.innerHTML = "";
 				    // Clear both arrays
 				    memValues = [];
@@ -140,5 +67,3 @@ function flipper(tile,val){
 		}
 	}
 }
-makeBoard()
->>>>>>> f96ef00edfc8ab5fb39380439c30ec2b13d76a99
