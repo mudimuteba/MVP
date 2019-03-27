a896555bc8a9fa1f40614c99867ed0d6bc071ca0
mudi <mudi.muteba@gmail.com>
2019-03-27 09:19:33 +0200

    refactor

--- app.js
+++ app.js
@@ -1,69 +1,47 @@
-var orgArr = ['壹','壹','貳','貳','叄','叄','肆','肆'];
-var memValues = [];
-var tileIds = [];
+let letters = ['壹','壹','貳','貳','叄','叄','肆','肆'];
+let pair = [];
+let tileIds = [];
+let tiles_flipped = 0;
 
-let shuffler = function(arr) {
-    let newPos, temp;
-
-    for(let i = arr.length - 1; i > 0; i--) {
-        newPos = Math.floor(Math.random() * (i+1));
-        temp = arr[i];
-        arr[i] = arr[newPos];
-        arr[newPos] = temp;
+function shuffler(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+	        let newPos = Math.floor(Math.random() * (i+1));
+	        [arr[i], arr[newPos]] = [arr[newPos], arr[i]];
         }
         return arr;
-};
+}
 
-// let newArray = shuffler(arry);
-// console.log(newArray);
-
-function startBoard(){
+function startBoard() {
 	tiles_flipped = 0;
-	var output = '';
-  shuffler(orgArr);
-  //for(var i = orgArr.length; i > 0; i--)
-  for(let i = 0; i < orgArr.length; i++)
-  {
-		output += '<div id="tile_'+i+'" onclick="flipper(this,\''+orgArr[i]+'\')"></div>';
-	}
-	document.getElementById('board').innerHTML = output;
+	let tileDiv = '';
+	shuffler(letters);
+	for (let i = 0; i < letters.length; i++)
+		tileDiv += '<div id="tile_'+i+'" onclick="flipper(this,\''+letters[i]+'\')"></div>';
+	document.getElementById('board').innerHTML = tileDiv;
 }
-function flipper(tile,val){
-	if(tile.innerHTML == "" && memValues.length < 2){
-		tile.style.background = '#FFF';
-		tile.innerHTML = val;
-		if(memValues.length == 0){
-			memValues.push(val);
-			tileIds.push(tile.id);
-		} else if(memValues.length == 1){
-			memValues.push(val);
-			tileIds.push(tile.id);
-			if(memValues[0] == memValues[1]){
-				tiles_flipped += 2;
-				memValues = [];
-        		tileIds = [];
-				if(tiles_flipped == orgArr.length){
 
-				function resetGame(){
-				let output = '<h1>🥇<u>Congrats you have won | 你赢了</u>🏆</h1></br><button onclick="startBoard()">Load New Game...</button>';
-				return output
-				}
-				document.getElementById('board').innerHTML = resetGame();
-				}
-			} else {
-				function flipBack(){
-				    var tile_1 = document.getElementById(tileIds[0]);
-				    var tile_2 = document.getElementById(tileIds[1]);
-				    tile_1.style.background = 'url(https://i.imgur.com/3mIrLAj.png) no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(https://i.imgur.com/3mIrLAj.png) no-repeat';
-            	    tile_2.innerHTML = "";
-				    memValues = [];
-            	    tileIds = [];
-				}
-				setTimeout(flipBack, 1400);
-			}
-		}
-	}
+function flipBack(){
+    var tile_1 = document.getElementById(tileIds[0]);
+    var tile_2 = document.getElementById(tileIds[1]);
+    tile_1.style.background = tile_2.style.background = 'url(https://i.imgur.com/3mIrLAj.png) no-repeat';
+    tile_1.innerHTML = tile_2.innerHTML = "";
+    [pair, tileIds] = [[], []];
 }
-//window.onload = startBoard();
+
+function flipper(tile, letter) {
+	if (tile.innerHTML == "" && pair.length < 2) {
+		tile.style.background = '#FFF';
+		tile.innerHTML = letter;
+
+		if (!pair[0] || !pair[1]) {
+			pair.push(letter);
+			tileIds.push(tile.id);
+		}
+		
+		if (pair[0] && pair[1])
+			pair[0] == pair[1] ? (tiles_flipped += 2, [pair, tileIds] = [[], []]) : setTimeout(flipBack, 1400);
+	}
+
+	if (tiles_flipped == letters.length)
+		document.getElementById('board').innerHTML = '<h1>🥇<u>Congrats you have won | 你赢了</u>🏆</h1></br><button onclick="startBoard()">Load New Game...</button>';
+}
